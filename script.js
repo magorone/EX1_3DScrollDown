c45bce0dd718fd08588c2eaab0b0d1c262b136fc
Emil <vyalshin.emil@gmail.com>
2024-08-21 12:41:25 +0300

    21/08/2924 12:41

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-function locomotive() {
-  gsap.registerPlugin(ScrollTrigger);
+function locomotive() 
+{
+	gsap.registerPlugin(ScrollTrigger);
 
-  const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("#main"),
-    smooth: true ,
-  });
-  locoScroll.on("scroll", ScrollTrigger.update);
+	const locoScroll = new LocomotiveScroll({
+		el: document.querySelector("#main"),
+		smooth: true ,
+	});
+	locoScroll.on("scroll", ScrollTrigger.update);
 
-  ScrollTrigger.scrollerProxy("#main", {
-    scrollTop(value) {
-      return arguments.length
-        ? locoScroll.scrollTo(value, 0, 0)
-        : locoScroll.scroll.instance.scroll.y;
-    },
+	ScrollTrigger.scrollerProxy("#main", 
+	{
+		scrollTop(value) {
+			return arguments.length
+				? locoScroll.scrollTo(value, 0, 0)
+				: locoScroll.scroll.instance.scroll.y;
+		},
 
-    getBoundingClientRect() {
-      return {
-        top: 0,
-        left: 0,
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
-    },
+		getBoundingClientRect() {
+			return {
+				top: 0,
+				left: 0,
+				width: window.innerWidth,
+				height: window.innerHeight,
+						};
+		},
 
-    pinType: document.querySelector("#main").style.transform
-      ? "transform"
-      : "fixed",
-  });
-  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-  ScrollTrigger.refresh();
+		pinType: document.querySelector("#main").style.transform
+			? "transform"
+			: "fixed",
+	});
+	ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+	ScrollTrigger.refresh();
 }
-locomotive();
 
+locomotive();
 
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
@@ -41,148 +43,134 @@ canvas.height = window.innerHeight;
 
 
 window.addEventListener("resize", function () {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  render();
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+render();
 });
 
 function files(index) 
 {
-  var data = ``;
+	var data = ``;
 
-  // RunFromSide
-  for (let index = 1; index <= 21; index++) 
-  {
-    if (index < 10) 
-      {data += `./img/RunFromSide/RunFromSide0` + index + `.png\n`;}
-    else if (index < 100)
-      {data += `./img/RunFromSide/RunFromSide` + index + `.png\n`;}
-      
-  } 
-  // RunSide
-  for (let index = 1; index <= 19; index++) 
-  {
-    if (index < 10) 
-      {data += `./img/RunSide/Run0` + index + `.png\n`;}
-    else if (index < 100)
-      {data += `./img/RunSide/Run` + index + `.png\n`;}
-  } 
-  
-  // RunSide60FPS
-  for (let index = 4; index <= 41; index++) 
-    {
-      if (index < 10) 
-        {data += `./img/RunSide60FPS/RunSide00` + index + `.png\n`;}
-      else if (index < 100)
-        {data += `./img/RunSide60FPS/RunSide0` + index + `.png\n`;}
-    } 
+	// RunFromSide
+	for (let index = 1; index <= 21; index++) 
+	{
+		if (index < 10) 
+			{data += `./img/RunFromSide/RunFromSide0` + index + `.png\n`;}
+		else if (index < 100)
+			{data += `./img/RunFromSide/RunFromSide` + index + `.png\n`;}
+			
+	} 
+	// RunSide
+	for (let index = 1; index <= 19; index++) 
+	{
+		if (index < 10) 
+			{data += `./img/RunSide/Run0` + index + `.png\n`;}
+		else if (index < 100)
+			{data += `./img/RunSide/Run` + index + `.png\n`;}
+	} 
 
+	// RunSide60FPS
+	for (let index = 4; index <= 41; index++) 
+		{
+			if (index < 10) 
+				{data += `./img/RunSide60FPS/RunSide00` + index + `.png\n`;}
+			else if (index < 100)
+				{data += `./img/RunSide60FPS/RunSide0` + index + `.png\n`;}
+		} 
 
-
-
-
-
-  for (let index = 1; index < 301; index++) 
-  {
-    if (index < 10) 
-    {data += `./images/male000` + index + `.png\n`;}
-    else if (index < 100)
-    {data += `./images/male00` + index + `.png\n`;}
-    else if (index < 1000)
-    {data += `./images/male0` + index + `.png\n`;}
-  }
-
-  return data.split("\n")[index];
+	return data.split("\n")[index];
 }
 
 const frameCount = 300;
 
 const images = [];
 const imageSeq = {
-  frame: 1,
+	frame: 1,
 };
 
 for (let i = 0; i < frameCount; i++) {
-  const img = new Image();
-  img.src = files(i);
-  images.push(img);
+	const img = new Image();
+	img.src = files(i);
+	images.push(img);
 }
 
 gsap.to(imageSeq, {
-  frame: frameCount - 1,
-  snap: "frame",
-  ease: `none`,
-  scrollTrigger: {
-    scrub: 0.15,
-    trigger: `#page>canvas`,
-    start: `top top`,
-    end: `600% top`,
-    scroller: `#main`,
-  },
-  onUpdate: render,
+		frame: frameCount - 1,
+		snap: "frame",
+		ease: `none`,
+		scrollTrigger: {
+		scrub: 0.15,
+		trigger: `#page>canvas`,
+		start: `top top`,
+		end: `600% top`,
+		scroller: `#main`,
+		},
+		onUpdate: render,
 });
 
 images[1].onload = render;
 
 function render() {
-  scaleImage(images[imageSeq.frame], context);
+	scaleImage(images[imageSeq.frame], context);
 }
 
 function scaleImage(img, ctx) {
-  var canvas = ctx.canvas;
-  var hRatio = canvas.width / img.width;
-  var vRatio = canvas.height / img.height;
-  var ratio = Math.max(hRatio, vRatio);
-  var centerShift_x = (canvas.width - img.width * ratio) / 2;
-  var centerShift_y = (canvas.height - img.height * ratio) / 2;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(
-    img,
-    0,
-    0,
-    img.width,
-    img.height,
-    centerShift_x,
-    centerShift_y,
-    img.width * ratio,
-    img.height * ratio
-  );
+	var canvas = ctx.canvas;
+	var hRatio = canvas.width / img.width;
+	var vRatio = canvas.height / img.height;
+	var ratio = Math.max(hRatio, vRatio);
+	var centerShift_x = (canvas.width - img.width * ratio) / 2;
+	var centerShift_y = (canvas.height - img.height * ratio) / 2;
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.drawImage(
+		img,
+		0,
+		0,
+		img.width,
+		img.height,
+		centerShift_x,
+		centerShift_y,
+		img.width * ratio,
+		img.height * ratio
+	);
 }
+
 ScrollTrigger.create({
-  trigger: "#page>canvas",
-  pin: true,
-  // markers:true,
-  scroller: `#main`,
-  start: `top top`,
-  end: `600% top`,
+	trigger: "#page>canvas",
+	pin: true,
+	// markers:true,
+	scroller: `#main`,
+	start: `top top`,
+	end: `600% top`,
 });
 
-
-
 gsap.to("#page1",{
-  scrollTrigger:{
-    trigger:`#page1`,
-    start:`top top`,
-    end:`bottom top`,
-    pin:true,
-    scroller:`#main`
-  }
+	scrollTrigger:{
+		trigger:`#page1`,
+		start:`top top`,
+		end:`bottom top`,
+		pin:true,
+		scroller:`#main`
+	}
 })
+
 gsap.to("#page2",{
-  scrollTrigger:{
-    trigger:`#page2`,
-    start:`top top`,
-    end:`bottom top`,
-    pin:true,
-    scroller:`#main`
-  }
+	scrollTrigger:{
+		trigger:`#page2`,
+		start:`top top`,
+		end:`bottom top`,
+		pin:true,
+		scroller:`#main`
+	}
 })
+
 gsap.to("#page3",{
-  scrollTrigger:{
-    trigger:`#page3`,
-    start:`top top`,
-    end:`bottom top`,
-    pin:true,
-    scroller:`#main`
-  }
+	scrollTrigger:{
+		trigger:`#page3`,
+		start:`top top`,
+		end:`bottom top`,
+		pin:true,
+		scroller:`#main`
+}
 })
